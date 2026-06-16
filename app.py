c8dded384e121abbfa303d6d261b227b7960b47e
Gustavo michelotto <gustavo.m.luz@aluno.senai.br>
2026-06-16 08:44:37 -0400

    Adicionado pagina de inimigos 2

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def inimigo():
     return render_template('inimigo.html')
 
+@app.route('/inimigo2')
+def inimigo2():
+    return render_template('inimigospt2.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
